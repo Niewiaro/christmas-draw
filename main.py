084419f417e49315a2ed16d8e31ac5915e1b11be
Jakub <56446320+Niewiaro@users.noreply.github.com>
2024-11-13 19:59:53 +0100

    chore: read from excel and send mail

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 import win32com.client as win32
+import pandas as pd
+from pathlib import Path
+
+
+class Config:
+    def __init__(self) -> None:
+        self.input_name = "input.xlsx"
+        self.input_path = Path.cwd() / self.input_name
 
 
 class Person:
-    def __init__(self, /, name: str, surname: str, email: str) -> None:
+    def __init__(self, *, name: str, email: str) -> None:
         self.name = name
-        self.surname = surname
         self.email = email
 
     def __repr__(self) -> str:
-        return f"{self.name}\t{self.surname}\t{self.email}"
+        return f"{self.name}\t{self.email}"
 
 
-def send_email(to: str, subject: str, body: str = None, html_body: str = None):
+def get_data_from_excel(path) -> list:
+    file = pd.ExcelFile(path)
+    with file as xlsx:
+        df = pd.read_excel(xlsx, "Sheet1")
+        # print(df.to_string())
+        result = [
+            Person(name=name, email=email)
+            for name, email in df[["Nazwa", "Adres e-mail"]].to_numpy()
+        ]
+        return result
+
+
+def send_email(
+    *, to: str, subject: str, body: str = None, html_body: str = None
+) -> None:
     outlook = win32.Dispatch("outlook.application")
     mail = outlook.CreateItem(0)
     mail.To = to
     mail.Subject = subject
 
     if body is not None:
-        mail.Body = "Message body"
+        mail.Body = body
 
     if html_body is not None:
-        mail.HTMLBody = "<h2>HTML Message body</h2>"
+        mail.HTMLBody = html_body
 
     mail.Send()
 
@@ -29,14 +50,10 @@ def send_email(to: str, subject: str, body: str = None, html_body: str = None):
 def main() -> None:
     print("https://github.com/Niewiaro/christmas-draw")
 
-    persons = [
-        Person(name="Jan", surname="Mak", email="jan.mak@example.com"),
-        Person(name="Anna", surname="Nowak", email="anna.nowak@example.com"),
-        Person(name="Piotr", surname="Ćwir", email="piotr.cwir@example.com"),
-    ]
-
+    config = Config()
+    persons = get_data_from_excel(config.input_path)
     for person in persons:
-        print(person)
+        send_email(to=person.email, subject="Test", body=f"Hello {person.name}")
 
 
 if __name__ == "__main__":
